8205b81f3b62b23e0e55842a35fddeb95b11668c
Hasbem <marvinmsb69@gmail.com>
2024-08-21 21:33:20 +0200

    ajout de prix dans data

--- assets/data/home.ts
+++ assets/data/home.ts
@@ -20,37 +20,46 @@ export const menus = [
   {
     text: "Poulet braisé entier",
     img: require("@/assets/data/c1.png"),
+    price: "23 €",
     id: "poulet",
   },
   {
     text: "Cuisse de poulet braisé",
     img: require("@/assets/data/c2.png"),
+    price: "18 €",
     id: "poulet",
   },
   {
     text: "Frites",
     img: require("@/assets/data/c3.png"),
+    price: "5 €",
     id: "accompagnement",
   },
   {
     text: "Pain baguette",
     img: require("@/assets/data/c4.png"),
+    price: "3 €",
+
     id: "accompagnement",
   },
   {
     text: "Oasis",
+    price: "2 €",
     id: "boisson",
   },
   {
     text: "Coca-Cola",
+    price: "2 €",
     id: "boisson",
   },
   {
     text: "Orangina",
+    price: "2 €",
     id: "boisson",
   },
   {
     text: "az",
+    price: "2 €",
     id: "boisson",
   },
 ];
