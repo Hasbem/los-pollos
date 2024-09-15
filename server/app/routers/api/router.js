516bb8e23361d11a441d804e29b9bb60bb4a31e8
Hasbem <marvinmsb69@gmail.com>
2024-09-15 13:32:10 +0200

    fix all app not working

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -1,14 +1,23 @@
 const express = require("express");
 
+const authController = require("../../controllers/authController");
+const {
+  getUserByEmail,
+  hashPassword,
+  verifyToken,
+} = require("../../services/authMiddleware");
+
 const router = express.Router();
 
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
 
-const itemsRouter = require("./items/router");
-
-router.use("/items", itemsRouter);
+// Routes pour l'authentification
+router.post("/login", getUserByEmail, authController.login);
+router.post("/users", hashPassword, userController.addUser);
+router.get("/logout", verifyToken, authController.logout);
+router.get("/verify-auth", verifyToken, authController.loginSuccess);
 
 /* ************************************************************************* */
 
